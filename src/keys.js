5c354d9a867c6041c6af4180c59c3941d31d53a8
Jose Calle <jose.callesoraluz@unmsm.edu.pe>
2021-12-13 00:47:02 -0500

    gaa

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,9 @@
 module.exports = {
    database: {
-      host: 'localhost',
-      user: 'Sebastian VBOOK',
-      password: 'sebasvbook',
-      database: 'vbook',
+      host: 'us-cdbr-east-05.cleardb.net',
+      user: 'b7fd6191524734',
+      password: '9992cba2',
+      database: 'heroku_a2fd79052888e7b',
    },
 };
+//mysql://b7fd6191524734:9992cba2@us-cdbr-east-05.cleardb.net/heroku_a2fd79052888e7b?reconnect=true
